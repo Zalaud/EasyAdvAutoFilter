2596c4f20036edf475bea632b71971e36fd8dc0f
Zalaud <158453522+Zalaud@users.noreply.github.com>
2024-03-12 20:00:24 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
-if [ -z https://github.com/Zalaud/EasyAdvAutoFilter ]
+if [ -z https://github.com/Zalaud/EasyAdvAutoFilter.git ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Zalaud/EasyAdvAutoFilter
+  git clone https://github.com/Zalaud/EasyAdvAutoFilter.git
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone https://github.com/Zalaud/EasyAdvAutoFilter
+  git clone https://github.com/Zalaud/EasyAdvAutoFilter.git
 fi
-cd /EasyAdvAutoFilter
+cd EasyAdvAutoFilter
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
